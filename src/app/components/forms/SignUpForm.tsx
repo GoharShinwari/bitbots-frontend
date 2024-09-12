6eb0c7978eaf3ab07a532f870d8af052615dcae4
Gohar Shinwari <121142075+GoharShinwari@users.noreply.github.com>
2024-09-12 00:25:18 -0400

    redirection

--- src/app/components/forms/SignUpForm.tsx
+++ src/app/components/forms/SignUpForm.tsx
@@ -91,7 +91,7 @@ export function SignupForm() {
         </Card>
         <div className="mt-4 text-center text-sm">
           Have an account?
-          <Link className="underline ml-2" href="signin">
+          <Link className="underline ml-2" href="/signin">
             Sign In
           </Link>
         </div>
--- src/app/components/forms/SigninForm.tsx
+++ src/app/components/forms/SigninForm.tsx
@@ -51,7 +51,7 @@ export function SigninForm() {
         </Card>
         <div className="mt-4 text-center text-sm">
           Don&apos;t have an account?
-          <Link className="underline ml-2" href="signup">
+          <Link className="underline ml-2" href="/signup">
             Sign Up
           </Link>
         </div>
